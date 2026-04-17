75ab493ea6230423e25be0174a1b15252cb0eefe
Rohit Kumar <rohit04kumar04rj@gmail.com>
2026-04-17 13:26:38 +0530

    new

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+
+const about
+ = () => {
+  return (
+    <div className='text-bold text-amber-400 bg-black flex justify-center items-center'>
+        this is about page
+    </div>
+  )
+}
+
+export default about
---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function rohit(){
+  return (
+    <div>not-found</div>
+  )
+}
+
